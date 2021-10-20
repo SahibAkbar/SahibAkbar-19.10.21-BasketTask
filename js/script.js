0cfdaad3cb04866726044f8845d97f798389154e
SahibAkbar <sahibaa@code.edu.az>
2021-10-20 07:00:20 +0400

    Fisrt

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let i = document.getElementById("icon")
+let tik = document.getElementById("tik");
+let contents = document.getElementById("content");
+
+i.addEventListener("click", () =>{
+    tik.classList.toggle("tosu");
+});
